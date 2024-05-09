16d836b1675961165d9bbbcfcd5440b3ecc637b2
SergioABS0813 <sergioandre.bustamante@gmail.com>
2024-05-08 19:20:05 -0500

    primer commit

---
+++ GTICS-LAB5/src/main/java/com/example/gticslab5/GticsLab5Application.java
@@ -0,0 +1,13 @@
+package com.example.gticslab5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GticsLab5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GticsLab5Application.class, args);
+    }
+
+}
---
+++ GTICS-LAB5/src/test/java/com/example/gticslab5/GticsLab5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gticslab5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GticsLab5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
