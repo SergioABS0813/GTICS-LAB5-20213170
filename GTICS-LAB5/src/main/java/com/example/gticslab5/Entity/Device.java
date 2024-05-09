ff08ce55e82180cdf9f36b0b7e0020f3826ce62b
SergioABS0813 <sergioandre.bustamante@gmail.com>
2024-05-08 20:20:28 -0500

    commit entities completo

--- GTICS-LAB5/src/main/java/com/example/gticslab5/Entity/Device.java
+++ GTICS-LAB5/src/main/java/com/example/gticslab5/Entity/Device.java
@@ -1,4 +1,29 @@
 package com.example.gticslab5.Entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "device")
 public class Device {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "DeviceID")
+    private int deviceId;
+    @Column(name = "DeviceName")
+    private String deviceName;
+    @Column(name = "DeviceType")
+    private String deviceType;
+    @Column(name = "Model")
+    private String model;
+    @Column(name = "SerialNumber")
+    private String serialNumber;
+    @Column(name = "SiteID")
+    private int siteId;
 }
--- GTICS-LAB5/src/main/java/com/example/gticslab5/Entity/Location.java
+++ GTICS-LAB5/src/main/java/com/example/gticslab5/Entity/Location.java
@@ -1,4 +1,23 @@
 package com.example.gticslab5.Entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "location")
 public class Location {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "LocationID")
+    private int locationId;
+    @Column(name = "City")
+    private String city;
+    @Column(name = "Country")
+    private String country;
 }
